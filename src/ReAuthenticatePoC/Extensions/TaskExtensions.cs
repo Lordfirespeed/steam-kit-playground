34b4284587455301b8d90697dd1ca0fee1658738
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-27 22:44:06 +0100

    add extension methods that wait for a task suppressing cancellation exceptions

---
+++ src/ReAuthenticatePoC/Extensions/TaskExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+
+namespace ReAuthenticatePoC.Extensions;
+
+public static class TaskExtensions
+{
+    public static async ValueTask SuppressingCancellation(this Task task)
+    {
+        await task.ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing);
+        if (task.IsFaulted) throw new Exception("", task.Exception!);
+    }
+
+    public static async ValueTask SuppressingCancellation<T>(this Task<T> task)
+    {
+        await SuppressingCancellation((Task)task);
+    }
+
+    public static async ValueTask SuppressingCancellation(this ValueTask task)
+    {
+        await SuppressingCancellation(task.AsTask());
+    }
+
+    public static async ValueTask SuppressingCancellation<T>(this ValueTask<T> task)
+    {
+        await SuppressingCancellation((Task)task.AsTask());
+    }
+}
