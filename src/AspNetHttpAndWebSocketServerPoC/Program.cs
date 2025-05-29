67a432c39ed8cb9c563721d65f5a8f48f968c7bd
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-05-29 22:31:13 +0100

    working server behind nginx reverse proxy

---
+++ src/AspNetHttpAndWebSocketServerPoC/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Threading;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Mono.Unix;
+
+var socketInfo = new UnixFileInfo("/tmp/steam-auth.sock");
+
+var builder = WebApplication.CreateSlimBuilder();
+builder.WebHost.ConfigureKestrel(options => {
+    options.ListenUnixSocket(socketInfo.FullName);
+});
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+var cancellationSource = new CancellationTokenSource();
+cancellationSource.CancelAfter(new TimeSpan(hours: 0, minutes: 1, seconds: 0));
+
+var runTask = app.RunAsync(cancellationSource.Token);
+
+await runTask;
